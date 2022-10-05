46ba52dd6ec21898b1b9305f3fed460c818f1344
Mikael Gueck <gumi@iki.fi>
2022-10-05 06:06:37 +0300

    header works

--- README.md
+++ README.md
@@ -22,6 +22,8 @@ https://www.scb.se/globalassets/vara-tjanster/px-programmen/px-file_format_speci
 
 https://www.scb.se/en/services/statistical-programs-for-px-files/px-file-format/
 
+https://www.scb.se/en/services/statistical-programs-for-px-files/px-file-format/keywords-with-long-texts-256-per-line/
+
 https://www.scb.se/px-en
 
 https://groups.google.com/g/pcaxis
@@ -38,3 +40,15 @@ https://statfin.stat.fi/database/StatFin/StatFin_rap.csv
 
 https://groups.google.com/g/pcaxis/c/mBH_2jh5rN0/m/liFBobXWCwAJ
 
+
+### Test data
+
+https://github.com/search?q=AXIS-VERSION+KEYS+extension%3Apx&type=Code
+
+https://github.com/ofurkusi/pxr-legacy/tree/master/pkg/tests
+
+https://github.com/r-forge/pxr/tree/master/pkg/tests
+
+https://github.com/cran/qmrparser/tree/master/inst/extdata
+
+https://github.com/statisticssweden/PxWeb/tree/master/PXWeb/Resources/PX/Databases/Example
--- cmd/pcaxis2parquet/main.go
+++ cmd/pcaxis2parquet/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"os"
 
 	"github.com/mikaelhg/gpcaxis/internal"
 )
 
 func main() {
-	f, err := os.Open("../data/010_kats_tau_101.px")
+	f, err := os.Open(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
-	pxParser := Parser{}
+	pxParser := Parser{
+		Headers: make(map[internal.PxHeaderKeyword]internal.PxHeaderValue),
+	}
 	pxParser.ParseHeader(f)
 
 }
@@ -26,59 +31,97 @@ type Parser struct {
 }
 
 func (p Parser) ParseDataDense() {
-	p.HeaderParserState.Count += 1
 }
 
-func (p Parser) ParseHeader(input *os.File) {
-
+func (p *Parser) ParseHeader(input *os.File) {
+	reader := bufio.NewReader(input)
+	buffer := make([]byte, 4096)
+out:
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			panic(err)
+		}
+		if n == 0 {
+			break
+		}
+		for i := 0; i < n; i++ {
+			stop, err := p.ParseHeaderCharacter(buffer[i])
+			if err != nil {
+				fmt.Printf("%#v\n", p.HeaderParserState)
+				panic(err)
+			}
+			if stop {
+				// rewind the reader
+				break out
+			}
+		}
+	}
+	fmt.Printf("%#v\n", p.HeaderParserState)
 }
 
-func (p Parser) ParseHeaderCharacter(c int8) bool {
-	inQuotes := p.HeaderParserState.Quotes%2 == 0
+func (p *Parser) ParseHeaderCharacter(c byte) (bool, error) {
+	inQuotes := p.HeaderParserState.Quotes%2 == 1
 	inParenthesis := p.HeaderParserState.ParenthesisOpen > p.HeaderParserState.ParenthesisClose
 	inKey := p.HeaderParserState.Semicolons == p.HeaderParserState.Equals
 	inLanguage := inKey && p.HeaderParserState.SquarebracketOpen > p.HeaderParserState.SquarebracketClose
 	inSubkey := inKey && inParenthesis
 
+	p.HeaderParserState.Count += 1
+
 	if c == '"' {
 		p.HeaderParserState.Quotes += 1
+
 	} else if (c == '\n' || c == '\r') && inQuotes {
-		println("There can't be newlines inside quoted strings.")
+		return true, errors.New("There can't be newlines inside quoted strings.")
+
 	} else if (c == '\n' || c == '\r') && !inQuotes {
-		return false
+		return false, nil
+
 	} else if c == '[' && inKey && !inQuotes {
 		p.HeaderParserState.SquarebracketOpen += 1
+
 	} else if c == ']' && inKey && !inQuotes {
 		p.HeaderParserState.SquarebracketClose += 1
+
 	} else if c == '(' && inKey && !inQuotes {
 		p.HeaderParserState.ParenthesisOpen += 1
+
 	} else if c == '(' && !inKey && !inQuotes {
 		// TLIST opening quote
 		p.HeaderParserState.ParenthesisOpen += 1
-		p.RowAccumulator.Value += c
+		p.RowAccumulator.Value += string(c)
+
 	} else if c == ')' && inKey && !inQuotes {
 		p.HeaderParserState.ParenthesisClose += 1
 		p.RowAccumulator.Subkeys = append(p.RowAccumulator.Subkeys, p.RowAccumulator.Subkey)
 		p.RowAccumulator.Subkey = ""
+
 	} else if c == ')' && !inKey && !inQuotes {
 		// TLIST closing quote
 		p.HeaderParserState.ParenthesisClose += 1
-		p.RowAccumulator.Value += c
+		p.RowAccumulator.Value += string(c)
+
 	} else if c == ',' && inSubkey && !inQuotes {
 		p.RowAccumulator.Subkeys = append(p.RowAccumulator.Subkeys, p.RowAccumulator.Subkey)
 		p.RowAccumulator.Subkey = ""
+
 	} else if c == ',' && !inKey && !inQuotes && !inParenthesis {
 		p.RowAccumulator.Values = append(p.RowAccumulator.Values, p.RowAccumulator.Value)
 		p.RowAccumulator.Value = ""
+
 	} else if c == '=' && !inKey && !inQuotes {
-		println("Found a second equals sign without a matching semicolon. Unexpected keyword terminator.")
+		return true, errors.New("Found a second equals sign without a matching semicolon. Unexpected keyword terminator.")
+
 	} else if c == '=' && inKey && !inQuotes {
 		if p.RowAccumulator.Keyword == "DATA" {
-			return true
+			return true, nil
 		}
 		p.HeaderParserState.Equals += 1
+
 	} else if c == ';' && inKey && !inQuotes {
-		println("Found a second equals sign without a matching semicolon. Unexpected keyword terminator.")
+		return true, errors.New("Found a second equals sign without a matching semicolon. Unexpected keyword terminator.")
+
 	} else if c == ';' && !inKey && !inQuotes {
 		if len(p.RowAccumulator.Value) > 0 {
 			p.RowAccumulator.Values = append(p.RowAccumulator.Values, p.RowAccumulator.Value)
@@ -86,16 +129,20 @@ func (p Parser) ParseHeaderCharacter(c int8) bool {
 		p.HeaderParserState.Semicolons += 1
 		p.Headers[p.RowAccumulator.ToKeyword()] = p.RowAccumulator.ToValue()
 		p.RowAccumulator = internal.RowAccumulator{}
-		return false
+		return false, nil
+
 	} else if inSubkey {
-		p.RowAccumulator.Subkey += c
+		p.RowAccumulator.Subkey += string(c)
+
 	} else if inLanguage {
-		p.RowAccumulator.Language += c
+		p.RowAccumulator.Language += string(c)
+
 	} else if inKey {
-		p.RowAccumulator.Keyword += c
+		p.RowAccumulator.Keyword += string(c)
+
 	} else {
-		p.RowAccumulator.Value += c
+		p.RowAccumulator.Value += string(c)
 	}
 
-	return false
+	return false, nil
 }
