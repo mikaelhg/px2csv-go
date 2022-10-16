51082f9f7a6cf3b531adafe3b3dc2d86fd7e8bcc
Mikael Gueck <gumi@iki.fi>
2022-10-16 09:08:42 +0300

    Minor cleanup.

--- cmd/pcaxis2parquet/main.go
+++ cmd/pcaxis2parquet/main.go
@@ -12,21 +12,23 @@ func main() {
 	pxFilename := flag.String("px", "", "PC-AXIS input file")
 	csvFilename := flag.String("csv", "", "CSV output file")
 	flag.Parse()
+
 	inf, err := os.Open(*pxFilename)
 	if err != nil {
 		panic(err)
 	}
 	defer inf.Close()
+
 	outf, err := os.OpenFile(*csvFilename, os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		panic(err)
 	}
 	defer outf.Close()
-	reader := bufio.NewReader(inf)
-	writer := bufio.NewWriter(outf)
+
+	reader, writer := bufio.NewReader(inf), bufio.NewWriter(outf)
+
 	pxParser := internal.Parser{}
 	pxParser.ParseHeader(reader)
 	pxParser.ParseDataDense(reader, writer)
 	writer.Flush()
-	outf.Close()
 }
